daa9f7c8d2540fda8b7746d4583a6db74a5b4367
root <2940544565@qq.com>
2021-11-26 16:18:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * git测试
+ *
+ * @author Administrator
+ * @date 2021/11/26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
