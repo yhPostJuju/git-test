1cd9580334fe570b0ca193a440c7081aaecfbe1a
root <2940544565@qq.com>
2021-11-26 16:27:57 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        System.out.println("hello Git");
+        System.out.println("hello Git2");
+        System.out.println("hello Git3");
     }
 }
